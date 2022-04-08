097509e934db564d91159cfba3959dbd136a04e0
Artsem <a.maliutsin@andersenlab.com>
2022-04-08 17:11:07 +0400

    task 1

---
+++ jobs/app.py
@@ -0,0 +1,10 @@
+# import from flask
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/jobs')
+@app.route('/')
+def jobs():
+    return render_template('index.html')
